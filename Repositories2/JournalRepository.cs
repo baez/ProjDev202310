62e4d9c405bb25e2100a7b703281915a7d71f736
GB Karim <george.bekari@gmail.com>
2023-11-06 21:33:15 -0800

    save latest

--- Repositories2/JournalRepository.cs
+++ Repositories2/JournalRepository.cs
@@ -5,16 +5,12 @@ namespace ProjDev220310.Examples
 {
     public class JournalRepository : IJournalRepository
     {
-
-
         public bool Save(string filename, Journal journal)
         {
-            // search to make sure there is no existing journal 
-            // being written to
             if (!File.Exists(filename))
             {
-            File.WriteAllText(filename, journal.ToString());
-            return true;
+                File.WriteAllText(filename, journal.ToString());
+                return true;
             }
 
             return false;
